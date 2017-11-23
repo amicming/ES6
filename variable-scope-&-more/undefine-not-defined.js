02d962c836ddb23e9eefcdeb66338f366db563d5
Amit Pandya <amicming@gmail.com>
2017-11-22 19:37:28 -0800

    undefined vs not defined

---
+++ variable-scope-&-more/undefine-not-defined.js
@@ -0,0 +1,6 @@
+// console.log(team);	// This will display `undefined`
+//  var team = '49ers';
+
+console.log(team);  // This will generate an error like Uncaught ReferenceError: team is not defined
+const team = '49ers';
+// Even if you declate `let`, you will get an error like team is not defined
